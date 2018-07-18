18defda7494c5a8dfbb162cc9b191e949ee242fc
prabhu-durasoft <iamprabhu@yahoo.com>
2018-07-18 15:35:05 +0530

    Day02 Web

---
+++ Day02Web/src/main/java/com/intuit/lab02/GameController.java
@@ -0,0 +1,50 @@
+package com.intuit.lab02;
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GameController {
+	@Autowired
+	private GuessingGame game;
+
+	@PostMapping("/play")
+	public String play(@RequestParam int guess, Model model) {
+		game.play(guess);
+		model.addAttribute("message", game.getMessage());
+		model.addAttribute("attempts", "Attempts: " + game.getAttempts());
+		if(game.isGameOver())
+			return "over";
+		return "game";
+	}
+	
+/*
+	@PostMapping("/play")
+	public String play(@RequestParam int guess, HttpSession session) {
+		game.play(guess);
+		session.setAttribute("message", game.getMessage());
+		session.setAttribute("attempts", "Attempts: " + game.getAttempts());
+		if(game.isGameOver())
+			return "over";
+		return "game";
+	}
+*/	
+	@GetMapping("/game")
+	public String index() {
+		return "game";
+	}
+
+}
+
+
+
+
+
+
